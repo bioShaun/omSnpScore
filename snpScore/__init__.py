a46c146e3fdf4459bf2fe79281a1dd19317e14ce
Bioshaun <ricekent@163.com>
2019-07-13 08:32:50 -0400

    add tableFromVcf vcf -> vcf table

---
+++ snpScore/__init__.py
@@ -0,0 +1,1 @@
+from ._loading import tableFromVcf
---
+++ snpScore/_utils.py
@@ -0,0 +1,19 @@
+import asyncio
+
+
+async def async_sh_job(cmd, sema):
+    with (await sema):
+        p = await asyncio.create_subprocess_shell(
+            cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.STDOUT)
+        return (await p.communicate())[0].splitlines()
+
+
+def async_batch_sh_jobs(cmd_list, thread=2):
+    semaphore = asyncio.Semaphore(thread)
+    if cmd_list:
+        loop = asyncio.get_event_loop()
+        coro_list = [async_sh_job(cmd, semaphore) for cmd in cmd_list]
+        loop.run_until_complete(asyncio.wait(coro_list))
+        loop.close()
