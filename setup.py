ea8bb742a6fa194533ab6af4544681fb61e13adc
brightrock <ricekent@163.com>
2021-03-06 12:22:25 +0800

    adapt to pip with public data

--- setup.py
+++ setup.py
@@ -2,50 +2,53 @@
 
 from setuptools import setup, find_packages
 
-version = '1.0dev'
+version = "1.0dev"
 
-msg = '''------------------------------
+msg = """------------------------------
 Installing omSnpScore version {}
 ------------------------------
-'''.format(version)
+""".format(
+    version
+)
 print(msg)
 
 setup(
-    name='omSnpScore',
+    name="omSnpScore",
     version=version,
-    author='lx Gui',
-    author_email='guilixuan@gmail.com',
-    keywords=['bioinformatics', 'NGS', 'Reseq', 'SNP'],
-    license='MIT',
+    author="lx Gui",
+    author_email="guilixuan@gmail.com",
+    keywords=["bioinformatics", "NGS", "Reseq", "SNP"],
+    license="MIT",
     packages=find_packages(),
     include_package_data=True,
     scripts=[
-        'scripts/table2csv-mp',
-        'scripts/splitVcf-mp',
-        'scripts/snpScore-bychr',
-        'scripts/snpScore-mp2',
-        'scripts/varDensityCompare',
-        'scripts/varDensityCompare-cli',
-        'scripts/snpFilter-cli',
-        'scripts/snpFilter-bychr',
-        'scripts/snpFilter-mp',
+        "scripts/publicSample",
+        "scripts/table2csv-mp",
+        "scripts/splitVcf-mp",
+        "scripts/snpScore-bychr",
+        "scripts/snpScore-mp2",
+        "scripts/varDensityCompare",
+        "scripts/varDensityCompare-cli",
+        "scripts/snpFilter-cli",
+        "scripts/snpFilter-bychr",
+        "scripts/snpFilter-mp",
     ],
     install_requires=[
-        'fire',
-        'click',
-        'pandas',
-        'loguru',
-        'delegator.py',
-        'pybedtools',
-        'numpy',
-        'tables',
-        'attrs',
-        'jinja2',
+        "fire",
+        "click",
+        "pandas",
+        "loguru",
+        "delegator.py",
+        "pybedtools",
+        "numpy",
+        "tables",
+        "attrs",
+        "jinja2",
     ],
 )
 
-msg = '''------------------------------
+msg = """------------------------------
 omSnpScore installation complete!
 ------------------------------
-'''
+"""
 print(msg)
